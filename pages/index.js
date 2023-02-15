e4bc7bc9890250cd550720415d75e009cc5e9594
Kartikay5849 <104310254+Kartikay5849@users.noreply.github.com>
2023-02-15 21:50:24 +0530

    fontUpdates

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
           props: {
             "--translateY": {
               from: "0",
-              to: `${25 * modifier}px`
+              to: `${50 * modifier}px`
             }
           }
         }).start();
