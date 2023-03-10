a37794a23638db7cafbd7d7c98c9e2679feb6028
priyanshuRj <priyanshurajput0071109@gmail.com>
2023-03-11 02:33:29 +0530

    Refactored code

--- Components/HomePage.jsx
+++ Components/HomePage.jsx
@@ -10,14 +10,8 @@ export default function HomePage() {
     
     return (
         <div>
-
-            {/* <video autoPlay="true" muted loop className="absolute top-0 w-full r-0">
-                <source src="Assets/bgVideo.mp4" type="video/mp4 "/>
-
-            {/* <video src="Assets/bgVideo.mp4" autoPlay> */}
-
             <main className=" mb-12 min-h-[75vh] md:min-h-[100vh] relative">
-                <div className="flex flex-col justify-center px-6 text-center text-white  sm:flex-row sm:px-16">
+                <div className="flex flex-col justify-center px-6 text-center text-white sm:flex-row sm:px-16">
                     <div  >
                         <div className=" font-[Azonix] drop-shadow-[0_15px_10px_rgba(180,83,9,1)] delay-150 duration-150 break-words text-5xl md:text-7xl text-amber-500 mt-60  mx-auto  ">
                             VIBHAV
--- Components/UI/Footer.jsx
+++ Components/UI/Footer.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 export default function Footer() {
   return (
     <div>
-      <footer className=" text-white pb-4 footer footer-center p-4 bg-base-300 text-base-content">
-  <div >
-    <p className='text-xl flex justify-center items-center'>Made with ❤️ by 
-    <Link href='/team' className=' cursor-pointer text-red-400 text-2xl'> Team Vibhav </Link>
-    </p>
-  </div>
-</footer>
+      <footer className="p-4 pb-4 text-white  footer footer-center bg-base-300 text-base-content">
+        <div>
+          <p className="flex items-center justify-center text-xl">
+            Made with ❤️ by
+            <Link
+              href="/team"
+              className="text-2xl text-red-400 cursor-pointer "
+            >
+              {" "}
+              Team Vibhav{" "}
+            </Link>
+          </p>
+        </div>
+      </footer>
     </div>
-  )
+  );
 }
