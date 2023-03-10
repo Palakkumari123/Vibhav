21a51056dcbe4703c2118f8378ff8d4e11bd72df
priyanshuRj <priyanshurajput0071109@gmail.com>
2023-03-11 00:16:04 +0530

    Updated Layout file

--- Components/UI/Layout.js
+++ Components/UI/Layout.js
@@ -3,7 +3,6 @@ import AboutUs from '../AboutUs';
 import FAQs from './FAQs';
 import Footer from './Footer';
 import Header from "./Header";
-import Footer from "./Footer";
 export default function Layout({children}) {
   return (
     <div>
