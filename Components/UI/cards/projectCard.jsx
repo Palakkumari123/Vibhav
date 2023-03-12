e08ffab9fa9abb20dbb571463b18904141c6b67d
Kartikay5849 <kartikaysambher@gmail.com>
2023-03-13 01:39:46 +0530

    homePageMobile

--- Components/UI/cards/projectCard.jsx
+++ Components/UI/cards/projectCard.jsx
@@ -59,7 +59,7 @@ export default function ProjectCard({ card }) {
                 </p>
               )}
               <Link
-                href={card.github}
+                href={card.github} target={"_blank"} 
                 className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                 <FaGithub className="m-1" size={20} />
