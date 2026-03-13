674ed7eb9a8250e6211bc7a9d8b176b9e8dbb62e
Palak Mehra <palak678656@gmail.com>
2026-03-13 23:58:59 +0530

    commit

--- Components/UI/Footer.jsx
+++ Components/UI/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
 
         <div>
           <p className="flex items-center justify-center text-md sm:text-2xl font-chakra">
-            Made with <span className="text-cyan-400">💚</span> by
+            Made with    <span className="text-cyan-400 mx-1">   💚  </span>    by
             <Link
               href="https://www.linkedin.com/company/team-vibhav/"
               className="sm:text-3xl text-xl text-[#78B159] cursor-pointer"
@@ -25,25 +25,3 @@ export default function Footer() {
     </div>
   );
 }
-// import React from "react";
-// import Link from "next/link";
-// export default function Footer() {
-//   return (
-//     <div className="h-40 flex items-end justify-center bg-none">
-//       <footer className="p-4  z-10  pb-4 mx-auto text-white  footer footer-center text-base-content">
-//         <div>
-//           <p className="flex items-center justify-center text-md sm:text-2xl font-chakra">
-//             Made with <span className="text-cyan-400">💚</span> by
-//             <Link
-//               href="https://www.linkedin.com/company/team-vibhav/"
-//               className="sm:text-3xl text-xl text-[#78B159] cursor-pointer "
-//             >
-//               {" "}
-//               Team Vibhav{" "}
-//             </Link>
-//           </p>
-//         </div>
-//       </footer>
-//     </div>
-//   );
-// }
